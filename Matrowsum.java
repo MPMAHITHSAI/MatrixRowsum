053d33583d9cce1e3c5e235f6d8ccce8bc24f34c
MPMAHITHSAI <mpmahithsai@gmail.com>
2023-07-18 09:40:52 +0530

    row sum

---
+++ Matrowsum.java
@@ -0,0 +1,34 @@
+package Rowsum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Matrowsum {
+	public static int[] Collsum(int arr[][],int N, int M) {
+		int ar[]=new int[M];
+		for(int j=0;j<M;j++) {
+			int sum=0;
+			for(int i=0;i<N;i++)
+			{
+				sum=sum+arr[j][i];			
+			}
+			ar[j]=sum;
+		}
+		return ar;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int arr[][]=new int[n][m];
+		for(int i=0;i<m;i++) {
+			for(int j=0;j<n;j++) {
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		int value[]=Collsum(arr,n,m);
+		System.out.println(Arrays.toString(value));
+
+	}
+}
